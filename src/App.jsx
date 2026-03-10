bc9acb821376cd6fcabc30bf786ef94cb0e2ab23
LeonardoTupinamba <leonardo_tupinamba@hotmail.com>
2026-03-10 17:46:56 -0400

    setup inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./componentes/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>My Daily Habits</h1>
+      <p>Gerencie seus hábitos diários de forma simples e visual</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <small>Módulo 04 _ Desenvolvimento Front-end com React - ITEAM</small>
+    </footer>
+  );
+}
+
+export default Footer;
