2c882ca184f9180f6300215d019a8f4a88001e08
LeonardoTupinamba <leonardo_tupinamba@hotmail.com>
2026-03-11 17:14:00 -0400

    parte 2 do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import Header from "./componentes/Header";
+import BemVindo from "./componentes/Bemvindo";
 import Footer from "./componentes/Footer";
+import SecaoHabitos from "./componentes/SecaoHabitos";
+import HabitList from "./componentes/HabitList";
 import "./App.css";
 
 function App() {
+  const habits = [
+    { id: 1, titulo: "Exercício", meta: 5, ativo: true, diasFeitos: 5 },
+    { id: 2, titulo: "Leitura", meta: 7, ativo: true, diasFeitos: 3 },
+    { id: 3, titulo: "Meditação", meta: 7, ativo: false, diasFeitos: 0 },
+    { id: 4, titulo: "Hidratação", meta: 7, ativo: true, diasFeitos: 6 },
+  ];
+
+  return (
+    <div>
+      <Header />
+      <BemVindo nomeUsuario="turma iteam" totalHabitos={habits.length} />
+      <SecaoHabitos titulo="Meus Hábitos">
+        <HabitList habits={habits} />
+      </SecaoHabitos>
+      <Footer />
+    </div>
+  );
+}
+
+/*function App() {
   return (
     <div>
       <h1>My Daily Habits</h1>
@@ -9,6 +33,6 @@ function App() {
       <Footer />
     </div>
   );
-}
+}*/
 
 export default App;
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,10 @@
+function Cabecalho({ titulo, descricao }) {
+  return (
+    <header>
+      <h1>{titulo}</h1>
+      <p>{descricao}</p>
+    </header>
+  );
+}
+
+export default Cabecalho;
